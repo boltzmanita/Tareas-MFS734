c6d464e18d118ca9e9f7918a64f5815fbe4bb3c1
boltzmanita <juliorv456@gmail.com>
2023-03-27 00:27:25 -0600

    Segundo inciso del ejercicio 2.

--- Tarea2/molecules/ejercicio2.sh
+++ Tarea2/molecules/ejercicio2.sh
@@ -1,3 +1,4 @@
+# Instrucción 1
 echo -n "" > Elementos-ordenados.log
 
 for file in  *.pdb
@@ -5,7 +6,10 @@ do
    cat $file >> Elementos-ordenados.log
 done
 
-for file in *t*.pdb; do     head -n 3 $file; done
+# Instrucción 2 
+for file in *t*.pdb; do     cut -c -14 $file; done
 
+# Instrucción 3: Falta terminar este inciso.
+for file in *t*.pdb; do     cut -c -14 $file; done
 
 
