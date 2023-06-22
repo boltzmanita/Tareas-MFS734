24467b2c293f5790a443f3e8743314368c5d81c2
boltzmanita <juliorv456@gmail.com>
2023-06-22 12:51:06 -0600

    Avances en el Ejercicio de Tarea 2

--- Tarea2/molecules/ejercicio2.sh
+++ Tarea2/molecules/ejercicio2.sh
@@ -12,4 +12,9 @@ for file in *t*.pdb; do     cut -c -14 $file; done
 # Instrucción 3: Falta terminar este inciso.
 for file in *t*.pdb; do     cut -c -14 $file; done
 
+# Instrucción 4 
+for file in *.pdb; 
+do	mkdir -p "${file%.*}"
+	echo > "${file%.*}"/README.md "Este archivo fue creado por Julio Ramirez"	
+done
 
---
+++ Tarea2/molecules/octane/README.md
@@ -0,0 +1,1 @@
+Este archivo fue creado por Julio Ramirez
